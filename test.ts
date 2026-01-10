08c0116dd24de4ad845889240f64dae52b90b3fb
Ali Raza <aaliraza007@hotmail.com>
2026-01-10 22:29:16 +0500

    remove credt

--- test.ts
+++
@@ -1,3 +0,0 @@
-console.log("hi dev");
-mongodb+srv://akuraza6_db_user:BM9qKe96SEc352BX@graphql.aeepzvd.mongodb.net/todest?retryWrites=true&w=majority
-// mongodb+srv://akuraza6_db_user:<db_password>@graphql.aeepzvd.mongodb.net/?appName=graphql
